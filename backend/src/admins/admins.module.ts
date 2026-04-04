b099b19f26f61a3bf3676a061235f69eaa98bc90
codeswindler <wilsonliam425@gmail.com>
2026-04-04 22:30:00 +0300

    fix: import SmsModule into AdminsModule to resolve UnknownDependenciesException

--- backend/src/admins/admins.module.ts
+++ backend/src/admins/admins.module.ts
@@ -4,9 +4,13 @@ import { Admin } from '../entities/admin.entity';
 import { Permission } from '../entities/permission.entity';
 import { AdminsService } from './admins.service';
 import { AdminsController } from './admins.controller';
+import { SmsModule } from '../sms/sms.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Admin, Permission])],
+  imports: [
+    TypeOrmModule.forFeature([Admin, Permission]),
+    SmsModule
+  ],
   providers: [AdminsService],
   controllers: [AdminsController],
   exports: [AdminsService],
